706f17f284a6c4e6245b77fe3a5344b230be580c
Kroca <khoazat1@gmail.com>
2021-03-02 22:28:43 +0300

    greeting activity

--- app/src/main/java/com/example/horoscrope/MainActivity.kt
+++ app/src/main/java/com/example/horoscrope/MainActivity.kt
@@ -1,31 +1,84 @@
 package com.example.horoscrope
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupSpinner()
+        setupSign()
     }
 
-    fun toastMe(view: View){
-        val myToast = Toast.makeText( this, "Hello, Toast!",Toast.LENGTH_SHORT)
+    fun setupSign() {
+        val selectedSign = intent.getStringExtra("sign");
+        println(selectedSign)
+        if (selectedSign != null) {
+            var signName: TextView = findViewById(R.id.signName)
+            val signs = resources.getStringArray(R.array.signs)
+            val id = selectedSign.toInt()
+            signName.text = signs[id]
+        }
+
+    }
+
+
+    fun setupSpinner() {
+        val spinner: Spinner = findViewById(R.id.gender)
+        val arrayAdapter =
+            ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, arrayOf("M", "Ж"))
+        spinner.adapter = arrayAdapter
+
+        spinner.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("not selected")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                println("selected")
+            }
+
+        }
+
+    }
+
+    fun toastMe(view: View) {
+        val myToast = Toast.makeText(this, "Hello, Toast!", Toast.LENGTH_SHORT)
         myToast.show()
     }
 
-   /* fun count(view: View){
-        var textView = findViewById<TextView>(R.id.count_num)
-        var countString = Integer.parseInt(textView.text.toString())
-        countString++
-        textView.setText(countString.toString())
+    fun showDatePicker(view: View) {
+        view as Button
+        val calendar = Calendar.getInstance()
+        DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+                view.text = "$day.$month.$year"
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
     }
 
-    fun random(view: View){
-        var textView = findViewById<TextView>(R.id.count_num)
-        var countString = (0..100).random()
-        textView.setText(countString.toString())
-    }*/
+    fun showSignsScreen(view: View) {
+        val intent = Intent(this, SignsSelectorActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
--- app/src/main/java/com/example/horoscrope/SplashActivity.kt
+++ app/src/main/java/com/example/horoscrope/SplashActivity.kt
@@ -7,14 +7,14 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
-    private val SPLASH_TIME_OUT:Long = 2000
+    private val SPLASH_TIME_OUT: Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
 
         Handler(Looper.getMainLooper()).postDelayed({
-                       startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, SPLASH_TIME_OUT)
     }
